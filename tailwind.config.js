da1f29a15b5a719744e96062a84a19e49d1a1acc
Matheus Andrade <matheuslincon50@gmail.com>
2022-01-07 17:41:59 -0300

    edited tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ module.exports = {
     purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     darkMode: false,
     theme: {
-      extends:{},
+      extends:{
+        screens: {
+        "2xl": "1500px",
+        },
+        keyframes: {
+          pulse: {
+            "0%, 100%": {
+              opacity: 1,
+            },
+            "50%": {
+              opacity: 0.5,
+            },
+          },
+        },
+        animation: {
+          pulse: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        },
+      },
+    },
+    fontFamily: {
+      body: ["Andada Pro", "serif"],
+      sans: ["ui-sans-serif", "system-ui"],
     },
     variants: {
       extends:{},
