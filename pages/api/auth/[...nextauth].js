bdd27306d322cb4c6eeb1a1385b2fbee5d1ec365
Matheus Andrade <matheuslincon50@gmail.com>
2022-01-08 12:02:54 -0300

    added next auth and created a custom login page

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify"
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      authorization:
+        "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-read-email,streaming,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state,user-read-recently-played,user-follow-read",
+    }),
+    // ...add more providers here
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+});
