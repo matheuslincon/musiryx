73913e62485090761d45180116373de18e58e621
Matheus Andrade <matheuslincon50@gmail.com>
2022-01-08 22:25:45 -0300

    created poster design

--- components/Poster.js
+++ components/Poster.js
@@ -1,7 +1,16 @@
-function Poster() {
-  return (
-    <div>
+import { BsFillPauseFill, BsFillPlayFill } from 'react-icons/bs';
 
+function Poster({ track }) {
+  return (
+    <div className="w-[260px] h-[360px] rounded-[50px] overflow-hidden relative text-white/80 cursor-pointer hover:scale-105 hover:text-white/100 transition duration-200 ease-out group mx-auto">
+      <img src={track.albumUrl} alt="" className="h-full w-full absolute inset-0 object-cover rounded-[50px] opacity-80 group-hover:opacity-100"/>
+
+      <div className="absolute bottom-10 inset-x-0 ml-4 flex items-center space-x-3.5">
+        <div className="h-10 w-10 bg-[#5d00b9] rounded-full flex items-center justify-center group-hover:bg-[#7F00FF] flex-shrink-0">
+          {/* <BsFillPauseFill className="text-xl"/> */}
+          <BsFillPlayFill className="text-xl ml-[1px]"/>
+        </div>
+      </div>
     </div>
   )
 }
